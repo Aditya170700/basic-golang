c98a04b8dd4fca6d1422284fedb2d21a4cd20b12
Aditya170700 <adityaric72@gmail.com>
2023-11-22 06:51:54 +0700

    string

---
+++ string.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Aditya")
+	fmt.Println("Ricki")
+	fmt.Println("Julianto")
+	fmt.Println(len("Julianto")) // 8
+	fmt.Println("Julianto"[0])   // 74 -> byte
+}
